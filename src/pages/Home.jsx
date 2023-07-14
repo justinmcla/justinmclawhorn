37755bb7f0b0f18c0ef4e096034b37b988436b05
Justin McLawhorn <jwmclawhorn@gmail.com>
2023-07-14 18:53:01 -0400

    add meta tags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import { Helmet } from "react-helmet";
+
 export default function Home() {
   return (
     <section>
+      <Helmet>
+        <meta name="description" content="Justin McLawhorn is a software engineer interested in authentication, authorization, cloud-first development, and software design principles."/>
+        <meta name="author" content="Justin Mclawhorn"/>
+        <meta name="canonical" content="https://justinmclawhorn.com"/>
+        <meta name="og:title" content="Justin McLawhorn - Software Engineer"/>
+        <meta name="og:type" content="website"/>
+        <meta name="og:image" content="https://jwmrjustinmclawhorn.blob.core.windows.net/images/Justin_McLawhorn_Natural_Bridge_c.png"/>
+        <meta name="og:url" content="https://justinmclawhorn.com"/>
+        <meta name="og:description" content="Justin McLawhorn is a software engineer interested in authentication, authorization, cloud-first development, and software design principles."/>
+        <title>Justin McLawhorn | Software Engineer</title>
+      </Helmet>
       <h2>Hi there.</h2>
       <p>Welcome to where I keep my blog posts on various topics in software engineering.</p>
       <p>Some topics of interest include:</p>
